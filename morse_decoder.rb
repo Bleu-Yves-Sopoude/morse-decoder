38ea7180cb1fa3714c0071b4dfd5ae13bcfaadb1
devendra-alt <devendramulewa171@gmail.com>
2023-09-14 15:29:37 +0530

    Added decode_word method

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -32,42 +32,17 @@ def decode(char)
   $morse_map.map {|key,val| return key if val==char }
 end
 
-output= decode('--..')
-
-puts output$morse_map = {
-  "A":".-",
-  "B":"-...",
-  "C":"-.-.",
-  "D":"-..",
-  "E":".",
-  "F":"..-.",
-  "G":"--.",
-  "H":"....",
-  "I":"..",
-  "J":".---",
-  "K":"-.-",
-  "L":".-..",
-  "M":"--",
-  "N":"-.",
-  "O":"---",
-  "P":".--.",
-  "Q":"--.-",
-  "R":".-.",
-  "S":"...",
-  "T":"-",
-  "U":"..-",
-  "V":"...-",
-  "W":".--",
-  "X":"-..-",
-  "Y":"-.--",
-  "Z":"--.." 
-}
-
-
-def decode(char)
-  $morse_map.map {|key,val| return key if val==char }
+def decode_word(string)
+  ans_string = ''
+  output_l1 = string.strip.split(/\s/)
+  output_l1.each do |c|
+    if c!=' '
+      ans_string << decode(c).to_s 
+    end
+  end
+  puts ans_string
 end
+ 
 
-output= decode('--..')
+decode_word('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
 
-puts output
