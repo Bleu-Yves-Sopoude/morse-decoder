0981c24e0f7b7167b41160cfffad49d0dd414662
devendra-alt <devendramulewa171@gmail.com>
2023-09-14 16:05:50 +0530

    Add decod_word method in MorseDecoder class

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -1,48 +1,32 @@
-$morse_map = {
-  "A":".-",
-  "B":"-...",
-  "C":"-.-.",
-  "D":"-..",
-  "E":".",
-  "F":"..-.",
-  "G":"--.",
-  "H":"....",
-  "I":"..",
-  "J":".---",
-  "K":"-.-",
-  "L":".-..",
-  "M":"--",
-  "N":"-.",
-  "O":"---",
-  "P":".--.",
-  "Q":"--.-",
-  "R":".-.",
-  "S":"...",
-  "T":"-",
-  "U":"..-",
-  "V":"...-",
-  "W":".--",
-  "X":"-..-",
-  "Y":"-.--",
-  "Z":"--.." 
-}
+# frozen_string_literal: false
 
 
-def decode(char)
-  $morse_map.map {|key,val| return key if val==char }
-end
-
-def decode_word(string)
-  ans_string = ''
-  output_l1 = string.strip.split(/\s/)
-  output_l1.each do |c|
-    if c!=' '
-      ans_string << decode(c).to_s 
-    end
+# * morse_decoder class
+class MorseDecoder
+  def initialize
+    @morse_map = {
+      "A": '.-', "B": '-...', "C": '-.-.', "D": '-..', "E": '.', "F": '..-.', "G": '--.',
+      "H": '....', "I": '..', "J": '.---', "K": '-.-',
+      "L": '.-..', "M": '--', "N": '-.', "O": '---', "P": '.--.', "Q": '--.-', "R": '.-.', 
+      "S": '...', "T": '-',
+      "U": '..-', "V": '...-', "W": '.--', "X": '-..-', "Y": '-.--', "Z": '--..'
+    }
   end
-  puts ans_string
+
+  def decode(char)
+    @morse_map.map { |key, val| return key if val == char }
+    ' '
+  end
+
+  def decode_word(string)
+    ans_string = ''
+    output_l1 = string.strip.split(/\s/)
+    output_l1.each do |c|
+      ans_string << decode(c).to_s if c != ' '
+    end
+    puts ans_string
+  end
 end
- 
 
-decode_word('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
-
+morse_decoder = MorseDecoder.new
+morse_decoder.decode_word('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
